58dca0446c3fc8ae56ec38186fa3e95f7d6cc76b
MonkModeMan <monkmodeman@gmail.com>
2025-05-26 14:59:28 +0900

    20250526最新更新

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
     domains: ['18.183.140.58'],
     unoptimized: true,
   },
-
+  experimental: {
+    // Vercel に SSG ではなく Node.js ランタイムでの実行を強制
+    appDir: false,
+    instrumentationHook: true,
+    typedRoutes: true
+  },
+  output: 'standalone', // ← これが重要
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
@@ -16,6 +22,6 @@ const nextConfig = {
     };
     return config;
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
