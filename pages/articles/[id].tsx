12c3e0767e104ca62e412ddaf6bbf38a78f66ba9
MonkModeMan <monkmodeman@gmail.com>
2025-05-30 08:48:52 +0900

    20250530最新更新

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -187,15 +187,17 @@ export default function ArticlePage({ article }: Props) {
             ご興味のある方は以下の採用情報をご確認ください。
           </p>
           <a
-            href="https://en-gage.net/raisex_jobs/"
+            href=""
+            className="engage-recruit-widget"
+            data-height="300"
+            data-width="500"
+            data-url="https://en-gage.net/raisex_jobs/widget/?banner=1"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              src="/public/recruit-banner.jpg"
-              alt="採用バナー"
-              className="w-full max-w-2xl h-auto mx-auto shadow-md rounded"
-            />
+            <script
+              src="https://en-gage.net/common_new/company_script/recruit/widget.js?v=74abd4d08c3f541ffc47d90ca4e4bec1babf87cd5ec5620798da6c97ecc886c7">
+            </script>
           </a>
         </div>
       </article>
