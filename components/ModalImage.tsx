799f6ae10ce76e55e7a73bb3b61fa65f9aacd9a4
MonkModeMan <monkmodeman@gmail.com>
2025-05-29 09:36:47 +0900

    20250529最新更新

--- components/ModalImage.tsx
+++ components/ModalImage.tsx
@@ -1,5 +1,7 @@
 // components/ModalImage.tsx
 
+'use client'
+
 import { useState } from 'react'
 import Image from 'next/image'
 
@@ -10,37 +12,42 @@ type ModalImageProps = {
   height?: number
 }
 
-export default function ModalImage({ src, alt = '', width = 1280, height = 720 }: ModalImageProps) {
+export default function ModalImage({
+  src,
+  alt = '',
+  width = 1280,
+  height = 720,
+}: ModalImageProps) {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
+      {/* 通常表示（幅100%・ピンボケ防止） */}
       <Image
         src={src}
         alt={alt}
         width={width}
         height={height}
-        unoptimized
         loading="lazy"
         decoding="async"
+        unoptimized
         className="cursor-zoom-in w-full h-auto"
         onClick={() => setIsOpen(true)}
       />
+
+      {/* モーダルオーバーレイ */}
       {isOpen && (
-        <div
-          className="modal-overlay"
-          onClick={() => setIsOpen(false)}
-        >
-          <div style={{ position: 'relative', width: '90vw', height: '90vh' }}>
-            <Image
-              src={src}
-              alt={alt}
-              fill
-              unoptimized
-              className="modal-img cursor-zoom-out object-contain"
-              priority
-            />
-          </div>
+        <div className="modal-overlay" onClick={() => setIsOpen(false)}>
+          <Image
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+            loading="eager"
+            decoding="sync"
+            unoptimized
+            className="modal-img cursor-zoom-out"
+          />
         </div>
       )}
     </>
