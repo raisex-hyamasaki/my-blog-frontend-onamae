ad1ecee8d07d3ee24e7e6c0384d595d5dbc30b75
MonkModeMan <monkmodeman@gmail.com>
2025-05-31 16:23:26 +0900

    20250531最新更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,26 @@ module.exports = {
     { pattern: /^modal-img$/ },
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            code: {
+              backgroundColor: '#fef3c7', // Tailwind: bg-yellow-100
+              color: '#000000',
+              fontSize: '0.75rem', // Tailwind: text-xs
+              padding: '0 0.25rem',
+              borderRadius: '0.25rem',
+              whiteSpace: 'normal',
+              fontFamily:
+                'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, monospace',
+            },
+            'code::before': { content: 'none' },
+            'code::after': { content: 'none' },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
