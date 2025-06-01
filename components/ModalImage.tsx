4ef5394781d5398c88d0c7cd8995d9f6c3f8df09
MonkModeMan <monkmodeman@gmail.com>
2025-06-01 18:47:45 +0900

    20250601最新更新

--- components/ModalImage.tsx
+++ components/ModalImage.tsx
@@ -1,22 +1,33 @@
 // components/ModalImage.tsx
 
-import Image from 'next/image'
+import Image, { ImageProps } from 'next/image'
 
 type ModalImageProps = {
   src: string
   alt?: string
-}
+  className?: string
+  unoptimized?: boolean
+  width?: number
+  height?: number
+} & Partial<Pick<ImageProps, 'width' | 'height' | 'className' | 'unoptimized'>>
 
-export default function ModalImage({ src, alt }: ModalImageProps) {
+export default function ModalImage({
+  src,
+  alt,
+  width = 800,
+  height = 600,
+  className = 'w-full h-auto cursor-zoom-in modal-img',
+  unoptimized = true,
+}: ModalImageProps) {
   return (
     <a href={src} target="_blank" rel="noopener noreferrer">
       <Image
         src={src}
         alt={alt || ''}
-        width={800}
-        height={600}
-        unoptimized
-        className="w-full h-auto cursor-zoom-in modal-img"
+        width={width}
+        height={height}
+        unoptimized={unoptimized}
+        className={className}
       />
     </a>
   )
