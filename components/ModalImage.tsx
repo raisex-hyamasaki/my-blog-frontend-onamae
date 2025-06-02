4ced35d62e040b7e2289730904f9f0f43649ed80
MonkModeMan <monkmodeman@gmail.com>
2025-06-02 17:08:54 +0900

    20250602最新更新

--- components/ModalImage.tsx
+++ components/ModalImage.tsx
@@ -1,23 +1,15 @@
 // components/ModalImage.tsx
 
 import Image, { ImageProps } from 'next/image'
-import { useState } from 'react'
-import Modal from 'react-modal'
 
-export type ModalImageProps = {
+type ModalImageProps = {
   src: string
   alt?: string
   className?: string
   unoptimized?: boolean
   width?: number
   height?: number
-  onOpenModal?: () => void
-  onCloseModal?: () => void
 } & Partial<Pick<ImageProps, 'width' | 'height' | 'className' | 'unoptimized'>>
-
-if (typeof window !== 'undefined') {
-  Modal.setAppElement('body')
-}
 
 export default function ModalImage({
   src,
@@ -26,23 +18,9 @@ export default function ModalImage({
   height = 600,
   className = 'mx-auto w-full max-w-[800px] h-auto cursor-zoom-in',
   unoptimized = true,
-  onOpenModal,
-  onCloseModal,
 }: ModalImageProps) {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const openModal = () => {
-    setIsOpen(true)
-    onOpenModal?.()
-  }
-
-  const closeModal = () => {
-    setIsOpen(false)
-    onCloseModal?.()
-  }
-
   return (
-    <>
+    <a href={src} target="_blank" rel="noopener noreferrer">
       <Image
         src={src}
         alt={alt}
@@ -50,28 +28,8 @@ export default function ModalImage({
         height={height}
         unoptimized={unoptimized}
         className={className}
-        onClick={openModal}
         style={{ cursor: 'zoom-in' }}
       />
-
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={closeModal}
-        contentLabel="Image Modal"
-        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-50"
-        overlayClassName="fixed inset-0 bg-black bg-opacity-70"
-        shouldCloseOnOverlayClick={true}
-      >
-        <Image
-          src={src}
-          alt={alt}
-          width={width}
-          height={height}
-          unoptimized={unoptimized}
-          className="mx-auto max-w-full max-h-screen rounded cursor-zoom-out"
-          onClick={closeModal}
-        />
-      </Modal>
-    </>
+    </a>
   )
 }
